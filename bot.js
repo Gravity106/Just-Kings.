c59c3820e751e0be57c97b2ea93216b506ff0f86
Gravity106 <43704049+Gravity106@users.noreply.github.com>
2018-10-08 01:20:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -327,7 +327,6 @@ client.on('message', async message => {
 
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
-
   let args = message.content.split(" ").slice(1);
 
   if (command == "ban") {
